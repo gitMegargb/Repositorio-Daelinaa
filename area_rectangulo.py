3c3ab5c6e590d48bce92c991f9887736ced183b3
Luis eduardo olvera marin <luiseduardoolveramarin@gmail.com>
2026-07-30 20:14:22 -0500

    primer trabajo

---
+++ area_rectangulo.py
@@ -0,0 +1,6 @@
+base = float(input("ingrese la base del rectangulo: "))
+altura = float(input("ingresa la altura del rectangulo: "))
+
+area = base * altura
+
+print("El area del rectangulo es: ", area)
